d8dabb91843a72423d5c9b61b7ad0c08e00eb19b
Mauricio Paez <cornopaez@gmail.com>
2017-05-02 19:16:39 -0400

    Added controller to project.

---
+++ controller/controller.js
@@ -0,0 +1,22 @@
+module.exports.set = function(app) {
+	app.get('/', function (req, res) {
+	  res.send('<p>App is up and running.</p>')
+	})
+
+	app.post('/webhook', function (req, res) {
+	  console.log(req.body);
+
+	  var response_data = {
+			"speech": "This is a response from the backend.",
+			"displayText": "This is a response from the backend.",
+			// "data": {},
+			// "contextOut": [],
+			"source": "FestServer",
+			// "followupEvent" : {}
+		};
+
+	  res.set('Content-Type', 'application/json');
+
+	  res.send(response_data);
+	})
+};
--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const controller = require ("./controller/controller.js");
+
+controller.set(app);
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-app.get('/', function (req, res) {
-  res.send('<p>App is up and running.</p>')
-})
-
-app.post('/webhook', function (req, res) {
-  console.log(req);
-
-  var response_data = {
-		"speech": "This is a response from the backend.",
-		"displayText": "This is a response from the backend.",
-		// "data": {},
-		// "contextOut": [],
-		"source": "FestServer",
-		// "followupEvent" : {}
-	};
-
-  res.set('Content-Type', 'application/json');
-
-  res.send(response_data);
-})
 
 app.listen(process.env.PORT || 3000);
 
