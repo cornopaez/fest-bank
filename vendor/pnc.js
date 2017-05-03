ffee928d9f3a4071f95652861eebd5a54e472bcc
Mauricio Paez <cornopaez@gmail.com>
2017-05-02 21:00:56 -0400

    Added switch for PNC logic.

--- vendor/pnc.js
+++ vendor/pnc.js
@@ -1,18 +1,28 @@
 // var http = require('http');
 
 var pncData;
+var whoAmI;
+var result = "";
 
 module.exports.set = function(data) {
 	pncData = data;
 };
 
-// // Find out what action it is
-// switch (pncData.action) {
-// 	case "login":
-// 		// If login, retrieve accounts for each person.
-// 		break;
-// 	default:
-// 		break;
-// }
+// Find out what action it is
+switch (pncData.action) {
+	case "login":
+		// If login, retrieve accounts for each person.
+		result = "Hi " + pncData.parameters.username;
+		break;
+	case "who_am_i":
+		if (whoAmI === "") {
+			result = "A person has no name.";
+		} else {
+			result = pncData.parameters.username;
+		}
+		break;
+	default:
+		break;
+}
 
-exports.resultText = () => "Hi" + pncData.parameters.username;
+exports.resultText = () => result;
