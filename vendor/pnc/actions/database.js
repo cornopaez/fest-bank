96a370b0150ee8206e6351237bbba6c3469720e3
Mauricio Paez <cornopaez@gmail.com>
2017-05-04 14:26:10 -0400

    Changed init declaration of var.

--- vendor/pnc/actions/database.js
+++ vendor/pnc/actions/database.js
@@ -1,14 +1,14 @@
 const request = require("request");
-var result = "";
+var result = {};
 
 module.exports.getData = function() {
 
 	request("http://mmpgh.com/API/checkBill.php?keyword=GAS&startDate=2016-05-01&endDate=2017-05-31", function (error, response, body) {
 	  if (error) throw new Error(error);
-	  result = body;
+	  // result = body;
 	  result = JSON.parse(body);
 	  console.log("This is the raw body: " + body);
-	  console.log("The type of the body is: " + typeof body);
+	  console.log("The type of the database result is: " + typeof result);
 	});
 }
 
