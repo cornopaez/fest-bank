b7ca7df78a73e17a6ea34f68986ff0457739f80f
Mauricio Paez <cornopaez@gmail.com>
2017-05-04 17:04:33 -0400

    Added sync-request.

--- vendor/pnc/actions/database.js
+++ vendor/pnc/actions/database.js
@@ -1,5 +1,6 @@
-const request = require("request");
-const mysql = require('mysql')
+// const request = require("request");
+// const mysql = require('mysql')
+const request = require("sync-request");
 var result = "";
 
 module.exports.getData = function(startDate, endDate, billType) {
@@ -24,15 +25,17 @@ module.exports.getData = function(startDate, endDate, billType) {
 	// connection.end()
 
 	var url = "http://mmpgh.com/API/checkBill.php?keyword=" + billType + "&startDate=" + startDate + "&endDate=" + endDate;
-	console.log(url);
+	result = request("GET", url);
 
-	request(url, function (error, response, body) {
-	  if (error) throw new Error(error);
-	  result = body;
-	  console.log(result);
-	  console.log(url);
-	  // exports.getResult = () => result;
-	});
+	// console.log(url);
+
+	// request(url, function (error, response, body) {
+	//   if (error) throw new Error(error);
+	//   result = body;
+	//   console.log(result);
+	//   console.log(url);
+	//   // exports.getResult = () => result;
+	// });
 }
 
 exports.getResult = () => result;
