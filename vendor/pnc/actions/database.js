5ec81735b7ad36fa03ffeb6fe88fea4afa7d83dd
Mauricio Paez <cornopaez@gmail.com>
2017-05-04 14:16:19 -0400

    Added casting of result to JSON.

--- vendor/pnc/actions/database.js
+++ vendor/pnc/actions/database.js
@@ -1,11 +1,12 @@
 const request = require("request");
-var result;
+var result = "";
 
 module.exports.getData = function() {
 
 	request("http://mmpgh.com/API/checkBill.php?keyword=GAS&startDate=2016-05-01&endDate=2017-05-31", function (error, response, body) {
 	  if (error) throw new Error(error);
 	  result = body;
+	  result = JSON.parse(body);
 	  console.log(body);
 	  console.log("The type of the body is: " + typeof body);
 	});
