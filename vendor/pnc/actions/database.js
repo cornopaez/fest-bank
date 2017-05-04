0e83ad6242fea2b6632334897364bcaef6331d38
Mauricio Paez <cornopaez@gmail.com>
2017-05-04 13:54:07 -0400

    Added a newer query.

--- vendor/pnc/actions/database.js
+++ vendor/pnc/actions/database.js
@@ -3,7 +3,7 @@ var result = "";
 
 module.exports.getData = function() {
 
-	request("http://mmpgh.com/API/checkBill.php?keyword=GAS&startDate=2017-05-01&endDate=2017-05-31", function (error, response, body) {
+	request("http://mmpgh.com/API/checkBill.php?keyword=GAS&startDate=2016-05-01&endDate=2017-05-31", function (error, response, body) {
 	  if (error) throw new Error(error);
 	  result = body;
 	  console.log(body);
