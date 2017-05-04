3a0d709129e0e1ca0ce97680c87cfa602dd148be
Mauricio Paez <cornopaez@gmail.com>
2017-05-04 15:09:36 -0400

    Added debig lang.

--- vendor/pnc/actions/database.js
+++ vendor/pnc/actions/database.js
@@ -1,10 +1,14 @@
 const request = require("request");
-var result = {};
+var result = "";
 
 module.exports.getData = function(startDate, endDate, billType) {
 
-	request("http://mmpgh.com/API/checkBill.php?keyword=" + billType + "&startDate=" + startDate + "&endDate=" + endDate, function (error, response, body) {
+	var url = "http://mmpgh.com/API/checkBill.php?keyword=" + billType + "&startDate=" + startDate + "&endDate=" + endDate;
+	console.log(url);
+
+	request(url, function (error, response, body) {
 	  if (error) throw new Error(error);
+	  console.log(result);
 	  result = body;
 	});
 }
